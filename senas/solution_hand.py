b54ace534a2585e418ffd2005a325f20d40b54b1
Jose Wannan <jwannan13@gmail.com>
2022-11-08 00:15:50 -0600

    Reconocimiento de patrones: UPDATED

--- senas/solution_hand.py
+++ senas/solution_hand.py
@@ -22,35 +22,31 @@ def hand_video(frame):
 
     results = hands.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
     
-    
-    if not results.multi_hand_landmarks:
-        hands.close()
-        return cv2.flip(frame, 1)
     image_hight, image_width, _ = image.shape
-    annotated_image = image.copy()
+    annotated_image = image
     
-    
-    for hand_landmarks in results.multi_hand_landmarks:
-        #print(
-        #    f'Index finger tip coordinates: (',
-        #    f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * image_width}, '
-        #    f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * image_hight})'
-        #)
-        mp_drawing.draw_landmarks(
-            annotated_image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+    #if results.multi_hand_landmarks:
+    #    for hand_landmarks in results.multi_hand_landmarks:
+            #print(
+            #    f'Index finger tip coordinates: (',
+            #    f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * image_width}, '
+            #    f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * image_hight})'
+            #)
+    #        print("")
+            # mp_drawing.draw_landmarks(annotated_image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
 
     ### implementacion de reconocimiento facial para identificar movimientos de manos, se estipula correlacionar la posicion
     ### de las manos con la referencia facial de los ojos y manos
 
-    face = mp_face_detection.FaceDetection(
-    model_selection=0, min_detection_confidence=0.5)
+    ##face = mp_face_detection.FaceDetection(
+    ##model_selection=0, min_detection_confidence=0.5)
 
-    annotated_image_1 = cv2.cvtColor(annotated_image, cv2.COLOR_BGR2RGB)
-    results_face = face.process(annotated_image_1)
+    ##annotated_image_1 = cv2.cvtColor(annotated_image, cv2.COLOR_BGR2RGB)
+    ##results_face = face.process(annotated_image_1)
 
-    if results_face.detections:
-      for detection in results_face.detections:
-        
-        mp_drawing.draw_detection(annotated_image_1, detection)
+    ##if results_face.detections:
+    ##  for detection in results_face.detections:
+    ##    print("")
+        #mp_drawing.draw_detection(annotated_image_1, detection)
     
-    return cv2.flip(annotated_image_1, 1)
+    return cv2.flip(image, 1)
